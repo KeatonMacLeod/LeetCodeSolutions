090f7986bff2aeb660e154799d6a6b1c0a22f0f2
KeatonMacLeod <macleodkeaton@gmail.com>
2017-04-12 01:25:20 -0500

    Adding Solutions

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -1,5 +1,4 @@
-
- //Definition for singly-linked list.
+//Definition for singly-linked list.
  struct ListNode {
     int val;
      ListNode *next;
@@ -21,7 +20,7 @@
 
 			 while (currL1 != nullptr || currL2 != nullptr)
 			 {
-
+				 //Get proper values for each linked list.
 				 if (currL1 == nullptr)
 					 currL1Val = 0;
 				 else
@@ -33,11 +32,13 @@
 
 				 int val = currL1Val + currL2Val;
 
+				 //Check if there was a carry-over from the previous addition.
 				 if (carry)
 					 val += 1;
 
 				 carry = false;
 
+				 //Set values and flags depending on the newly computed value.
 				 if (val >= 10)
 				 {
 					 carry = true;
@@ -51,24 +52,28 @@
 					 }
 				 }
 
+				 //If this is the first node, point the whole newList to it.
 				 if (newList == nullptr)
 				 {
 					 newList = new ListNode(val);
 					 newListAdder = newList;
 				 }
 
+				 //Otherwise, get our newListAdder to append additional listNodes.
 				 else
 				 {
 					 newListAdder->next = new ListNode(val);
 					 newListAdder = newListAdder->next;
 				 }
 
+				 //Advance our pointers in our linked lists.
 				 if (currL1 != nullptr)
 					 currL1 = currL1->next;
 				 if (currL2 != nullptr)
 					 currL2 = currL2->next;
 			 }
 
+			 //If there was a carry in the last addition of the loop, append the carried "1".
 			 if (carry)
 			 {
 				 newListAdder->next = new ListNode(1);
