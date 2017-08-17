b19551d15f4bf0a62a2bc188afbea4184cb807a0
Keaton MacLeod <macleodkeaton@gmail.com>
2017-08-17 04:25:10 -0500

    Update and rename MergeKSortedLists to MergeKSortedLists.cpp

--- MergeKSortedLists.cpp
+++ MergeKSortedLists.cpp
@@ -1,3 +1,5 @@
+/*Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.*/
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
